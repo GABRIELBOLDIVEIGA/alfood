006591b533c724df3eba912faaa8f8aee62451d6
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-15 23:31:38 -0300

    ...

--- src/componentes/NavBar/index.tsx
+++ src/componentes/NavBar/index.tsx
@@ -10,6 +10,9 @@ const NavBar = () => {
       <li>
         <Link to="/restaurantes">Restaurantes</Link>
       </li>
+      <li>
+        <Link to="/admin">Area ADM</Link>
+      </li>
     </ul>
   </nav>)
 }
